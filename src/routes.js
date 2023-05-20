0c4e03ac1311db4b493230b4e731e02576078b03
irianto <good.pratama@gmail.com>
2023-05-20 15:00:49 +0800

    menampilkan catatan

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,30 @@
-const {addNoteHandler} = require('./handler');
+const {addNoteHandler, getAllNotesHandler, getNoteByIdHandler} = require('./handler');
 
 const routes = [
   {
     method: 'POST',
     path: '/notes',
     handler: addNoteHandler,
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
     options: {
       cors: {
         origin: ['*'],
